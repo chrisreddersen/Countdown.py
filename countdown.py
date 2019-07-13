b64e10b3f73d1da98cce3e5a1ad2f1f9c4bbc531
Chris Reddersen <39147103+chrisreddersen@users.noreply.github.com>
2019-07-13 07:29:04 -0600

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 08:55:33 2018
+
+@author: chris
+"""
+
+def main():
+  number = 11
+  while number <= 11 and number > 1:
+    number = number - 1
+    print(number)
+  
+  print("Bang! Your dead")
+main()
